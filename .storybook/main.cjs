13b51d1760ac525424d8625e7b38da10dc0ea0c9
Luis Henrique da Silva Campos <luis.henrique_campos@outlook.com.br>
2022-11-16 23:31:46 -0300

    ci: fix Checkout

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -6,7 +6,8 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "@storybook/addon-a11y"
   ],
   "framework": "@storybook/react",
   "core": {
@@ -15,12 +16,12 @@ module.exports = {
   "features": {
     "storyStoreV7": true
   },
-  // viteFinal: (config, {configType}) =>{
-  //   if (configType =='PRODUCTION'){
-  //     config.base = '/ignite-lab-system/'
-  //   }
+  viteFinal: (config, {configType}) =>{
+    if (configType =='PRODUCTION'){
+      config.base = '/ignite-lab-system/'
+    }
 
 
-  //   return config
-  // }
+    return config
+ }
 }
